fe7ac7087fbe1c3663a78054a10962486bdcba66
Gaetan RENAULT <gadetan@gmail.com>
2022-11-22 11:15:45 +0100

    WIP seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,34 +7,57 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'open-uri'
 require 'json'
+require 'faker'
 
 puts "Cleaning up database..."
 User.destroy_all
+Planet.destroy_all
 puts "Database cleaned"
 
-User.create!(
-  name: "Zeus",
-  email: "zeus@example.com"
+puts 'Creating 20 fake users...'
+gaetan = User.create!(
+  email: "gaetan@example.com",
+  password: "password",
+  name: "Gaetan Renault"
 )
-
-User.create!(
-  name: "Posseidon",
-  email: "posseidon@example.com"
+quentin = User.create!(
+  email: "quentin@example.com",
+  password: "password",
+  name: "Gaetan Renault"
 )
-
-User.create!(
-  name: "Thor",
-  email: "thor@example.com"
+guillaume = User.create!(
+  email: "guillaume@example.com",
+  password: "password",
+  name: "Gaetan Renault"
 )
-
-User.create!(
-  name: "Brian",
-  email: "brian@example.com"
-)
-
-
-
 puts "Users created"
 
 # adresse mail : user@example.com
 # login : password
+
+puts 'Creating 20 fake planets...'
+20.times do
+  planet = Planet.new(
+    name:    Faker::Space.planet,
+    localisation: Faker::Space.galaxy,
+    capacity: rand(1..20)*1000000000,
+    description: Faker::Books::Dune.quote,
+    price_per_millenia: rand(1..20)*1000000,
+    rating:  rand(0..5),
+    owner: [guillaume, gaetan, quentin].sample
+  )
+  planet.save!
+end
+puts 'Planets created'
+
+
+
+
+
+
+# create_table "bookings", force: :cascade do |t|
+#   t.integer "departure_date"
+#   t.integer "arrival_date"
+#   t.string "validate", default: "unvalidate"
+#   t.integer "total_price"
+#   t.big
