dab2f6d772c0448bee50a6312680c9c656df5dc2
Guillaume Messieux <guillaume.messieux@gmail.com>
2022-11-24 17:49:13 +0100

    add styling to booking index/ confirm and decline action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,7 @@
 class BookingsController < ApplicationController
   def index
     # @bookings = Booking.all
-    @bookings = current_user.bookings
+    @bookings = current_user.bookings.order(updated_at: :desc)
   end
 
   def create
