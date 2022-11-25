2e62da769d0389c574863a4019abcdec8ecd4810
Quentin Orhant <quentin.orhant@gmail.com>
2022-11-25 15:38:45 +0100

    done

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -25,7 +25,7 @@ export default class extends Controller {
       customMarker.className = "marker"
       customMarker.style.backgroundImage = `url('${marker.image_url}')`
       customMarker.style.backgroundSize = "contain"
-      customMarker.style-color = "blue"
+      customMarker.style.color = "blue"
       customMarker.style.width = "25px"
       customMarker.style.height = "25px"
 
